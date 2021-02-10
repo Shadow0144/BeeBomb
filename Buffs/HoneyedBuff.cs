2d8661173bc41a8d3035decb13eae41a1857294e
Shadow0144 <CorbinCogswell@gmail.com>
2021-02-10 13:55:00 -0600

    Bomb effects are all added, but do not appear to work on NPCs correctly

---
+++ Buffs/HoneyedBuff.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeeBomb.Buffs
+{
+    public class HoneyedBuff : ModBuff
+    {
+        private const float SPEED_MOD = 0.5f;
+
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Honey'd");
+            Description.SetDefault("Slows movement.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.moveSpeed *= SPEED_MOD;
+        }
+    }
+}
--- Projectiles/BeeBombProjectile.cs
+++ Projectiles/BeeBombProjectile.cs
@@ -5,14 +5,19 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.DataStructures;
+using BeeBomb.Buffs;
 
 namespace BeeBomb.Projectiles
 {
     public class BeeBombProjectile : ModProjectile
     {
-        private const int TIMER = 55;
+        private const int TIMER = 155;
         private const int RADIUS = 10;
+        private const int BEES = 12;
         private const int BEES_SPEED = 1;
+        private const int BEES_DAMAGE = 100;
+        private const float BEES_KNOCKBACK = 10.0f;
+        private const int HONEY_TIMER = 3000;
 
         public override void SetStaticDefaults()
 		{
@@ -23,7 +28,7 @@ namespace BeeBomb.Projectiles
 		{
 			projectile.width = 16;
 			projectile.height = 24;
-			projectile.aiStyle = 16;
+            projectile.CloneDefaults(ProjectileID.StickyBomb);
             aiType = ProjectileID.StickyBomb;
             projectile.timeLeft = TIMER;
         }
@@ -45,36 +50,34 @@ namespace BeeBomb.Projectiles
         {
             Vector2 position = projectile.Center;
             Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
-            int radius = 4;
-            int bees = 12; 
             
-            ExplosionDust(radius, projectile.Center, shake: false);
             ExplosionDamage();
 
-            for (int x = -radius; x <= radius; x++)
+            /*const int RADIUS = 4;
+            for (int x = -RADIUS; x <= RADIUS; x++)
             {
-                for (int y = -radius; y <= radius; y++)
+                for (int y = -RADIUS; y <= RADIUS; y++)
                 {
                     //int xPosition = (int)(x + position.X / 16.0f);
                     //int yPosition = (int)(y + position.Y / 16.0f);
-                    if (Math.Sqrt(x * x + y * y) <= radius + 0.5)
+                    if (Math.Sqrt(x * x + y * y) <= RADIUS + 0.5)
                     {
                         //WorldGen.KillTile(xPosition, yPosition, false, false, false);
                         Dust.NewDust(position, 22, 22, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);
                     }
                     else { }
                 }
-            }
+            }*/
 
             Random random = new Random();
-            for (int i = 0; i < bees; i++)
+            for (int i = 0; i < BEES; i++)
             {
                 int projectileID = Projectile.NewProjectile(projectile.Center, 
                     new Vector2(
                         ((float)random.NextDouble()) * BEES_SPEED * (random.NextDouble() > 0.5 ? 1 : -1), 
                         ((float)random.NextDouble()) * BEES_SPEED * (random.NextDouble() > 0.5 ? 1 : -1)),
-                        ProjectileID.Bee, 350, 10);
-                Main.projectile[projectileID].friendly = true;
+                        ProjectileID.Bee, BEES_DAMAGE, BEES_KNOCKBACK);
+                //Main.projectile[projectileID].friendly = true;
             }
         }
 
@@ -86,6 +89,7 @@ namespace BeeBomb.Projectiles
                 if (((dist / 16.0f) <= RADIUS) && (!npc.friendly))
                 {
                     int dir = (dist > 0) ? 1 : -1;
+                    npc.AddBuff(ModContent.BuffType<HoneyedBuff>(), HONEY_TIMER, false);
                     npc.StrikeNPC(projectile.damage, projectile.knockBack, dir);
                 }
             }
@@ -98,53 +102,18 @@ namespace BeeBomb.Projectiles
                 int dir = (dist > 0) ? 1 : -1;
                 if (((dist / 16.0f) <= RADIUS) && (Main.netMode == NetmodeID.SinglePlayer))
                 {
+                    player.AddBuff(ModContent.BuffType<HoneyedBuff>(), HONEY_TIMER, false);
                     player.Hurt(PlayerDeathReason.ByProjectile(player.whoAmI, projectile.whoAmI), (int)(projectile.damage * 1), dir); // 1 - never crit
                     player.hurtCooldowns[0] += 15;
                 }
                 else if ((Main.netMode != NetmodeID.MultiplayerClient) && ((dist / 16.0f) <= RADIUS))
                 {
+                    player.AddBuff(ModContent.BuffType<HoneyedBuff>(), HONEY_TIMER, false);
                     NetMessage.SendPlayerHurt(projectile.owner, PlayerDeathReason.ByProjectile(player.whoAmI, projectile.whoAmI), (int)(projectile.damage * 1), dir, false, pvp: true, 0); // 1 - never crit
                 }
                 else { }
             }
 
-        }
-
-        public static void ExplosionDust(int Radius, Vector2 Center, Color color = default, Color cloudColor = default, int type = 1, Vector2 Direction = default, bool shake = true, int dustType = 6)
-        {
-            // Check to see if the type is 1, 2 or 3, else default to 1
-            List<int> types = new List<int> { 1, 2, 3, 4 };
-
-            if (!types.Contains(type))
-            {
-                type = 1;
-            }
-            else { }
-
-            //What to preform once the type has been found
-            switch (type)
-            {
-                case 1:
-                    //Type1Dust(Radius, Center, color, cloudColor, DustType: dustType);
-                    break;
-                case 2:
-                    //Type2Dust(Radius, Center, color, cloudColor, Direction);
-                    break;
-                case 3:
-                    //Type3Dust(Radius, Center, shader, color, cloudColor, DustType: dustType);
-                    break;
-                case 4:
-
-                    break;
-                default:
-
-                    break;
-            }
-
-            // Lighting - brief flash
-            Lighting.AddLight(Center, new Vector3(Radius / 2.3f, Radius / 2.3f, Radius / 2.3f));
-            Lighting.maxX = Radius / 10;
-            Lighting.maxY = Radius / 10;
         }
     }
 }
